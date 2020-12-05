8464a11a743bb2650c268129421b2060a50281d1
Hassam Ali <hassam.ali@keeptruckin.com>
2020-12-05 21:31:41 +0500

    add chunk solution

--- udemy-algo-ds/chunk/index.js
+++ udemy-algo-ds/chunk/index.js
@@ -9,7 +9,26 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-
+  let chunked = [];
+  for(let item of array) {
+    const last = chunked[chunked.length - 1];
+    if (!last || last.length == size) {
+      chunked.push([item]);
+    } else last.push(item);
+  }
+  return chunked
 }
 
 module.exports = chunk;
+
+
+// function chunk(array, size) {
+//   let chunk = [];
+//   let counter = 0;
+
+//   while (counter < array.length) {
+//     chunk.push(array.slice(counter, counter + size));
+//     counter += size;
+//   }
+//   return chunk
+// }
