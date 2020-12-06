5e0db6c0f80f88c724091b49742229510652567a
Hassam Ali <hassam.ali@keeptruckin.com>
2020-12-06 20:08:44 +0500

    update debounce

--- frontendmaster-frontend/debounce-immediate/index.js
+++ frontendmaster-frontend/debounce-immediate/index.js
@@ -1,15 +1,47 @@
 function debounce(fn, waitTime, immediate) {
-  let timerId;
+  let timer;
   return function() {
-    const later = () => {
-      timerId = null;
-      if (!immediate) fn.apply(this, arguments);
+    clearTimeout(timer);
+    if (immediate && !timer) fn.call(this, ...arguments);
+
+    timer = setTimeout(() => {
+      timer = null
+      if (!immediate) fn.call(this, ...arguments);
+    }, waitTime);
+  }
+}
+
+function debounceIm(fn, waitTime) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    if (!timer) {
+      fn.call(this, ...arguments);
     }
-    const callNow = immediate && !timerId;
-    clearTimeout(timerId);
-    timerId = setTimeout(later, waitTime);
-    if (callNow) fn.apply(this, arguments);
+    timer = setTimeout(() => {
+      if (!timer) fn.call(this, ...arguments);
+      timer = null;
+    }, waitTime);
   }
 }
 
 module.exports = debounce;
+
+
+/*
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		clearTimeout(timeout);
+		//Moving this line above timeout assignment
+		if (immediate && !timeout) func.apply(context, args);
+		timeout = setTimeout(function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		}, wait);
+	};
+};
+
+
+*/
